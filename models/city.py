5e325f3a7da1e53e79ed35b8a679c7b9a7423703
symonmuchemi <simonkenya846@gmail.com>
2024-06-04 17:26:42 +0300

    fixed console.py

--- models/city.py
+++ models/city.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-from base_model import Base
+from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
 from sqlalchemy import ForeignKey
--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
-from base_model import Base
+from models.base_model import Base
 from sqlalchemy import String
 from sqlalchemy import Column
 from sqlalchemy.orm import relationship
