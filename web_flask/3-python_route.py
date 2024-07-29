80ca0795478e30fe612e8064cba9cb57f648f47a
symonmuchemi <simonkenya846@gmail.com>
2024-07-29 16:54:40 +0300

    Task 3: python route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Simple program script"""
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """print greeting
+
+    Returns:
+        str: returned string
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_string(text):
+    """Displays a special C string"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python/', defaults={'text': 'is fun'}, strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_route(text):
+    """python route
+
+    Args:
+        text (str): a string
+
+    Returns:
+        str: Python is @text
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
