60b18f0629e6be69c090af0dcfadd70eef8e2348
symonmuchemi <simonkenya846@gmail.com>
2024-07-29 17:13:01 +0300

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""Simple program script"""
+from flask import Flask
+from flask import url_for
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """print greeting
+
+    Returns:
+        str: returned string
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_string(text):
+    """Displays a special C string"""
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """python route
+
+    Args:
+        text (str): a string
+
+    Returns:
+        str: Python is @text
+    """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """number route
+
+    Args:
+        n (int): a number
+
+    Returns:
+        str: n is a number
+    """
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
