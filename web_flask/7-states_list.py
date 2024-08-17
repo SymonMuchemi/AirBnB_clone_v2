1fce7f22997c51d0f0e20df50509114801cf6ef7
symonmuchemi <simonkenya846@gmail.com>
2024-08-17 21:43:23 +0300

    fix: some changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,8 +3,8 @@
 from flask import Flask
 from flask import url_for
 from flask import render_template
-from models import *
 from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
@@ -18,8 +18,9 @@ def remove_sql_session(exception):
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
+    # states = sorted(list(storage.all(State).values()), key=lambda x: x.name)
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states, n=len(states))
 
 
 if __name__ == "__main__":
